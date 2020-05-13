211cf5f7216919bac51948bc2a48c30c26a667aa
dalwrigh <dalwrigh@cisco.com>
2020-05-13 13:48:03 -0400

    Added some testing

--- survey/tests.py
+++ survey/tests.py
@@ -3,6 +3,7 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from .models import Question
+from django.urls import reverse
 
 
 class QuestionModelTest(TestCase):
@@ -12,4 +13,33 @@ class QuestionModelTest(TestCase):
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
 
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
 
+    def test_was_published_recently_with_recent_question(self):
+        time = timezone.now()
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse('survey:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls available")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        create_question(question_text="Past question", days=-30)
+        response = self.client.get(reverse('survey:home'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question>']
+        )
