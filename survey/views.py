8685a7124e6bec9b09e2568a7fe114bb1529e382
dalwrigh <dalwrigh@cisco.com>
2020-05-13 13:02:41 -0400

    Very basic functionality achieved

--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+
+from .models import Choice, Question
+
+
+class HomeView(generic.ListView):
+    template_name = 'survey/home.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'survey/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'survey/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'survey/detail.html', {
+            'question': question,
+            'error_message': "Please select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('survey:results', args=(question_id,)))
